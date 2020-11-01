4afdbe2899f4ca1e23a7455e0128414e32915c16
Lee Prout <wleeprout@gmail.com>
2020-11-01 12:49:37 -0500

    data analysis 2

--- EmployeeSQL/employee_data_analysis.sql
+++ EmployeeSQL/employee_data_analysis.sql
@@ -2,6 +2,7 @@
 SELECT * 
 FROM "Employees";
 
+--Data Analysis #1 
 --Perform an Inner Join on Employees and Salaries Tables
 --matching key is emp_no
 --run to confirm join
@@ -17,5 +18,18 @@ FROM "Employees" AS e
 JOIN "Salaries" AS s
 ON e.emp_no = s.emp_no
 ORDER BY s.salary
+
+--Data Analysis #2
+--find names and hire dates for employees hired in 1986
+--all columns avail in Employees table
+--found this site helpful: https://www.postgresqltutorial.com/postgresql-between/
+SELECT first_name, last_name, hire_date
+FROM "Employees"
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31'
+ORDER BY hire_date
+
+--Data Analysis #3
+--manager of each dept
+
 
 
